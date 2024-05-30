6e9079431912d5861da07bd8fde4f489e983c087
Kate L <llewellyn.ksj@gmail.com>
2024-05-30 11:21:23 +0100

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+
+// Bootstrap
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div>
-        <h1 className="text-center">Purple Leaf</h1>
-      </div>
+      <Container>
+        <Routes>
+          <Route path="/" element={<h1 className="text-center">Home</h1>} />
+          <Route
+            path="/about"
+            element={<h1 className="text-center">About</h1>}
+          />
+          <Route
+            path="/support"
+            element={<h1 className="text-center">Support</h1>}
+          />
+          <Route
+            path="/contact"
+            element={<h1 className="text-center">Contact</h1>}
+          />
+          <Route
+            path="/login"
+            element={<h1 className="text-center">Login</h1>}
+          />
+          <Route
+            path="/register"
+            element={<h1 className="text-center">Register</h1>}
+          />
+        </Routes>
+      </Container>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,26 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Purple Leaf</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand href="#home">Purple Leaf</Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Support</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/support">Support</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
           </Nav>
           <Nav className="ml-auto">
-            <Nav.Link href="#link">Register</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
+            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/login">Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
