c4d44e0554c5dcfb01b97cc33d8043cc148888d4
Kate L <llewellyn.ksj@gmail.com>
2024-05-30 11:47:43 +0100

    Install Axios and add axios defaults

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,67 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+// Bootstrap
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router-dom";
+
+// CSS
+import s from "../styles/NavBar.module.css";
 
 const NavBar = () => {
+  const getNavLinkClass = ({ isActive }, additionalClasses = "") =>
+    isActive
+      ? `${s.NavColor} ${s.Active} ${additionalClasses}`
+      : `${s.NavColor} ${additionalClasses}`;
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <NavLink to="/">
+        <NavLink to="/" className={s.NavLink}>
           <Navbar.Brand href="#home">Purple Leaf</Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/support">Support</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink
+              className={(navData) => getNavLinkClass(navData, s.NavLink)}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={(navData) => getNavLinkClass(navData, s.NavLink)}
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className={(navData) => getNavLinkClass(navData, s.NavLink)}
+              to="/support"
+            >
+              Support
+            </NavLink>
+            <NavLink
+              className={(navData) => getNavLinkClass(navData, s.NavLink)}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
           </Nav>
           <Nav className="ml-auto">
-            <NavLink to="/register">Register</NavLink>
-            <NavLink to="/login">Login</NavLink>
+            <NavLink
+              className={(navData) => getNavLinkClass(navData, s.NavLink)}
+              to="/login"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              className={(navData) => getNavLinkClass(navData, s.NavLink)}
+              to="/register"
+            >
+              Register
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
